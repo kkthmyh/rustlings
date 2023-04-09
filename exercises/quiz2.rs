ad62b434620eb4ba11cf901ed9def9c354767e1b
kkthmyh <kkth.xj@gmail.com>
2023-04-09 23:48:09 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -30,12 +29,40 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Command::Trim => {
+                    let items: Vec<&str> = string.split(' ').collect();
+                    let items: Vec<&str> = items.into_iter().filter(|item| *item != "").collect();
+                    let mut s = "".to_string();
+                    let size = items.len();
+                    let mut index = 0;
+                    for item in items.iter() {
+                        s.push_str(*item);
+                        if index != size-1 {
+                            s.push_str(" ");
+                        }
+                        index += 1;
+                    }
+                    output.push(s);
+                }
+                Command::Append(n) => {
+                    let mut s = string.clone();
+                    for i in 0..*n {
+                        s.push_str("bar");
+                    }
+                    output.push(s);
+                }
+            }
         }
+        println!("{:?}", output);
         output
     }
 }
@@ -43,7 +70,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
